2a73889d03ac0fbd09276a80bf23ae478cfe6ee1
Scott Loarie <loarie@gmail.com>
2015-08-13 22:37:18 -0700

    altered crop function

--- R/crop_function.R
+++ R/crop_function.R
@@ -1,17 +1,49 @@
-#' A Cat Function
+#' Crop Function
 #'
-#' This function allows you to express your love of cats.
-#' @param love Do you love cats? Defaults to TRUE.
-#' @keywords cats
+#' This function takes responses (y), and 2 environmental variables (x1 & x2) 
+#' along with prior parameters for beta (b0, Vbcoef) and sigma2 (s1,s2)
+#' and produces estimates for beta and sigma using a Gibb's sampler
+#' and quadradic Bayesian regression
+#' @param b0, Vbcoef, s1, s2, y, x1, x2
+#' @keywords crop Bayesian model
 #' @export
 #' @examples
-#' cat_function()
+#' crop_function()
 
-crop_function <- function(love=TRUE){
-    if(love==TRUE){
-        print("I love cats!")
-    }
-    else {
-        print("I am not a cool person.")
-    }
+crop_function <- function(b0, Vbcoef, s1, s2, y, x1, x2){
+  library( mvtnorm )
+  library( MCMCpack )
+  
+  n = length( y )
+  X = cbind( rep( 1, times = n ), x1, x2, x1^2, x2^2 )
+  p = dim( X )[2]
+
+  #beta prior
+  Vb = Vbcoef * diag( p )
+  b_val = rmvnorm( 1, b0, Vb )[1,]
+
+  #sigma2 prior
+  sigma2_val = rinvgamma( 1, s1, s2 )
+
+  ngibbs = 10000
+  b_vals = matrix( NA, ngibbs, p )
+  sigma2_vals = rep( NA, times = ngibbs )
+  for( g in 1 : ngibbs ){
+    #sample sigma2
+    u1 = s1 + n / 2
+    u2 = s2 + 0.5 * crossprod( y - X %*% b_val )
+    sigma2_val = rinvgamma( 1, u1, u2 )
+    sigma2_vals[g] = sigma2_val 
+
+    #sample b
+    v = 1 / ( sigma2_val ) * crossprod( X, y ) + solve( Vb ) %*% b0
+    V_inv = 1/(sigma2_val) * crossprod(X) + solve( Vb )
+    b_val = rmvnorm( 1, solve( V_inv ) %*% v, solve( V_inv ) )[1,]
+    b_vals[g,] = b_val
+  }
+
+  b_mean = apply( b_vals, 2, mean )
+  sigma2_mean = mean( sigma2_vals )
+  print(paste("{\"beta\":[",paste(b_mean,collapse=","),"], \"sigma2\":",sigma2_mean,"}",sep="")[1])
+    
 }
