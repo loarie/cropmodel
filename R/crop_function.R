74c30f84381fd97fc70fcdd5f7cccde9bfcd2e48
Scott Loarie <loarie@gmail.com>
2015-08-16 11:20:20 -0700

    changed MCMC algorithm

--- R/crop_function.R
+++ R/crop_function.R
@@ -1,4 +1,3 @@
-#' Crop Function
 #'
 #' This function takes responses (y), and 2 environmental variables (x1 & x2) along with prior parameters for beta (b0, Vbcoef) and sigma2 (s1,s2) and produces estimates for beta and sigma using a Gibb's sampler and quadradic Bayesian regression
 #' @param b0, Vbcoef, s1, s2, y, x1, x2
@@ -7,41 +6,45 @@
 #' @examples
 #' crop_function()
 
-crop_function <- function(b0, Vbcoef, s1, s2, y, x1, x2){
-  library( mvtnorm )
-  library( MCMCpack )
-  
-  n = length( y )
-  X = cbind( rep( 1, times = n ), x1, x2, x1^2, x2^2, 2015 )
-  p = dim( X )[2]
+crop_function <- function(b0, Vbcoef, s1, s2, y1, x1, x2){
+  library(bayesm)
+  library(coda)
 
-  #beta prior
-  Vb = Vbcoef * diag( p )
-  b_val = rmvnorm( 1, b0, Vb )[1,]
+  n = length( y1 )
 
-  #sigma2 prior
-  sigma2_val = rinvgamma( 1, s1, s2 )
-
-  ngibbs = 20000
-  b_vals = matrix( NA, ngibbs, p )
-  sigma2_vals = rep( NA, times = ngibbs )
-  for( g in 1 : ngibbs ){
-    #sample sigma2
-    u1 = s1 + n / 2
-    u2 = s2 + 0.5 * crossprod( y - X %*% b_val )
-    sigma2_val = rinvgamma( 1, u1, u2 )
-    sigma2_vals[g] = sigma2_val 
-
-    #sample b
-    v = 1 / ( sigma2_val ) * crossprod( X, y ) + solve( Vb ) %*% b0
-    V_inv = 1/(sigma2_val) * crossprod(X) + solve( Vb )
-    b_val = rmvnorm( 1, solve( V_inv ) %*% v, solve( V_inv ) )[1,]
-    b_vals[g,] = b_val
+  if(n==1){ #simulate another data point from the model
+    y1 = c(y1, sum(b0*c(1, x1, x2, x1^2, x2^2, 115)))
+    x1 = c(x1,x1)
+    x2 = c(x2,x2)
+    n = 2
   }
+  
+  X1 = cbind( rep( 1, times = n ), x1, x2, x1^2, x2^2, 115 )
+  p = dim( X1 )[2]
 
-  b_mean = apply( b_vals, 2, mean )
-  sigma2_mean = mean( sigma2_vals )
-  #print(paste("{\"beta\":[",paste(b_mean,collapse=","),"], \"sigma2\":",sigma2_mean,"}",sep="")[1])
+  Data1 = list(y=y1,X=X1)
+
+  # I have manually set the prior information to the function default:
+  betabar1 = b0 # prior mean
+  A1 = Vbcoef*diag(p) # prior precision matrix (precision = 1/variance)
+
+  nu1 = s1
+  ssq1 = s2
+
+  Prior1 = list(betabar=betabar1,A=A1,nu=nu1,ssq=ssq1)
+
+  # This portion specifies MCMC parameters
+  R1 = 100000 # This is how many iterations to run the Gibbs sampler 
+  keep1 = 1 # This is the thinning parameter. 1 means no thinning.
+
+  MCMC1 = list(R=R1,keep=keep1)
+
+  ### Run the Gibbs sampler! ###
+  simOut = runiregGibbs(Data1,Prior1,MCMC1)
+
+  b_mean = apply( simOut$betadraw, 2, mean )
+  sigma2_mean = mean(simOut$sigmasqdraw)
   print(paste("{beta:[",paste(b_mean,collapse=","),"], sigma2:",sigma2_mean,"}",sep="")[1])
-    
+  print(sum(b0*c(1, x1[1], x2[1], x1[1]^2, x2[1]^2, 115)))
+  print(sum(b_mean*c(1, x1[1], x2[1], x1[1]^2, x2[1]^2, 115)))
 }
